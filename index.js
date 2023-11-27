774b86f2955766b4b6a307dc71f2ad8d4dde23c0
Prachi Gupta <117148255+prachiguptadev@users.noreply.github.com>
2023-11-27 17:34:43 +0000

    using array method

---
+++ index.js
@@ -0,0 +1,7 @@
+// using array method
+const first = 'abcde'.split("");    //string convert into array
+
+first.reverse()                     //using reverse method (array reverse)
+first.splice(2,1,"m")               //using splice
+
+console.log(first)
